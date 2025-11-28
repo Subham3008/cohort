1f9576394a25a1dcd80270809db96bd85797bfca
Subham3008 <subhamsamanta065@gmail.com>
2025-11-28 13:19:33 +0530

    first commit

---
+++ frontend_32_textConvertion/script.js
@@ -0,0 +1,31 @@
+let p = document.querySelector("p")
+let charecter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+let text = p.innerText
+
+let intervalId;
+
+p.addEventListener("mouseenter", () => {
+  clearInterval(intervalId)
+  iteration = 0
+
+  intervalId = setInterval(() => {
+    let str = text.split('').map((char, idx) => {
+      if (idx < iteration) {
+        return char
+
+      }
+      return charecter[Math.floor(Math.random() * charecter.length)]
+    }).join("")
+
+    p.innerText = str
+
+    iteration += 0.25
+
+    if (iteration >= text.length) {
+      clearInterval(intervalId)
+    }
+
+  }, 30)
+})
+
+
