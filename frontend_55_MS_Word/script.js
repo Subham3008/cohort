53d0f4dda45c86ee5b274fcc84ec7c5f53a587ce
Subham3008 <subhamsamanta065@gmail.com>
2026-03-09 14:52:08 +0530

    second commit

--- frontend_55_MS_Word/script.js
+++ frontend_55_MS_Word/script.js
@@ -1,11 +1,16 @@
 const wordNavP = document.querySelectorAll(".word-nav p")
 const allToolbar = document.querySelectorAll(".toolbar")
+const page = document.querySelector("#page")
 
 const bold = document.querySelector("#bold");
 const italic = document.querySelector("#italic");
 const underline = document.querySelector("#underline");
 const fontIncrease = document.querySelector("#fontIncrease");
 const fontDecrease = document.querySelector("#fontDecrease");
+const pageColorBtn = document.querySelector("#pageColor");
+const pageColor = document.querySelector(".pageColor");
+const colorCloseBtn = document.querySelector("#colorClose");
+const allColorBtn = document.querySelectorAll(".color");
 
 
 {/*--word nav items toggle and show toolbar items-- */ }
@@ -27,28 +32,36 @@ wordNavP.forEach(elem => {
 {/*---home toolbar---*/ }
 
 function getSelectionRange() {
-
   const selection = window.getSelection();
-
   if (!selection.rangeCount) return null;
-
   return selection.getRangeAt(0);
-
 }
 
-function applyFormat(style, value){
+function applyFormat(style, value) {
 
+  const selection = window.getSelection();
   const range = getSelectionRange();
-  if(!range) return;
+  if (!range) return;
+
+  const parent = selection.anchorNode.parentElement;
+
+  // toggle off
+  if (parent.style[style] === value) {
+    parent.style[style] = "normal";
+    return;
+  }
 
   const span = document.createElement("span");
-
   span.style[style] = value;
 
   span.appendChild(range.extractContents());
-
   range.insertNode(span);
 
+  // 🔥 selection restore
+  selection.removeAllRanges();
+  const newRange = document.createRange();
+  newRange.selectNodeContents(span);
+  selection.addRange(newRange);
 }
 
 bold.addEventListener("click", () => {
@@ -63,3 +76,22 @@ underline.addEventListener("click", () => {
   applyFormat("textDecoration", "underline");
 });
 
+
+{/*--Design--*/ }
+{/*--page color--*/ }
+
+pageColorBtn.addEventListener('click', function () {
+  pageColor.style.top = "100px"
+})
+
+colorCloseBtn.addEventListener('click', function () {
+  pageColor.style.top = "-100%"
+})
+
+{/*--all btn color--*/ }
+allColorBtn.forEach(btn => {
+  btn.style.backgroundColor = btn.dataset.color;
+  btn.addEventListener('click', function () {
+    page.style.backgroundColor = btn.dataset.color
+  })
+});
