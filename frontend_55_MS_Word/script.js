2eff4fa41b6bfcf2302ac46861338d39957f0c18
Subham3008 <subhamsamanta065@gmail.com>
2026-03-09 23:19:26 +0530

    3rd commit

--- frontend_55_MS_Word/script.js
+++ frontend_55_MS_Word/script.js
@@ -1,6 +1,9 @@
 const wordNavP = document.querySelectorAll(".word-nav p")
 const allToolbar = document.querySelectorAll(".toolbar")
-const page = document.querySelector("#page")
+const page = document.querySelector(".page")
+const pageBorder = document.querySelector(".page-border")
+const pageSize = document.querySelector("#pageSize");
+
 
 const bold = document.querySelector("#bold");
 const italic = document.querySelector("#italic");
@@ -11,6 +14,13 @@ const pageColorBtn = document.querySelector("#pageColor");
 const pageColor = document.querySelector(".pageColor");
 const colorCloseBtn = document.querySelector("#colorClose");
 const allColorBtn = document.querySelectorAll(".color");
+const borderCloseBtn = document.querySelector("#borderClose");
+const pageBordersBtn = document.querySelector("#pageBorders");
+const borderPanel = document.querySelector(".border-panel");
+
+const borderStyle = document.querySelector("#borderStyle");
+const borderWidth = document.querySelector("#borderWidth");
+const borderColor = document.querySelector("#borderColor");
 
 
 {/*--word nav items toggle and show toolbar items-- */ }
@@ -81,7 +91,12 @@ underline.addEventListener("click", () => {
 {/*--page color--*/ }
 
 pageColorBtn.addEventListener('click', function () {
-  pageColor.style.top = "100px"
+
+  if (pageColor.style.top === "100px") {
+    pageColor.style.top = "-100%";
+  } else {
+    pageColor.style.top = "100px";
+  }
 })
 
 colorCloseBtn.addEventListener('click', function () {
@@ -89,9 +104,100 @@ colorCloseBtn.addEventListener('click', function () {
 })
 
 {/*--all btn color--*/ }
-allColorBtn.forEach(btn => {
+allColorBtn.forEach(function (btn) {
   btn.style.backgroundColor = btn.dataset.color;
   btn.addEventListener('click', function () {
     page.style.backgroundColor = btn.dataset.color
   })
 });
+
+pageBordersBtn.addEventListener('click', function () {
+  if (borderPanel.style.top === "100px") {
+    borderPanel.style.top = "-100%";
+  } else {
+    borderPanel.style.top = "100px"
+  }
+})
+
+borderCloseBtn.addEventListener('click', function () {
+  borderPanel.style.top = "-100%"
+})
+
+/*--border design--*/
+function updateBorder() {
+
+  pageBorder.style.borderStyle = borderStyle.value;
+  pageBorder.style.borderWidth = borderWidth.value + "px";
+  pageBorder.style.borderColor = borderColor.value;
+
+}
+
+borderStyle.addEventListener("change", updateBorder);
+borderWidth.addEventListener("input", updateBorder);
+borderColor.addEventListener("input", updateBorder);
+
+/*--layout--*/
+/*--page size selection--*/
+const pageSizesArr = [
+  {
+    name: "A3",
+    value: "a3",
+    width: "1123px",
+    height: "1587px"
+  },
+  {
+    name: "A4",
+    value: "a4",
+    width: "794px",
+    height: "1123px"
+  },
+  {
+    name: "A5",
+    value: "a5",
+    width: "559px",
+    height: "794px"
+  },
+  {
+    name: "Letter",
+    value: "letter",
+    width: "816px",
+    height: "1056px"
+  },
+  {
+    name: "Legal",
+    value: "legal",
+    width: "816px",
+    height: "1344px"
+  }
+];
+
+pageSize.addEventListener('change', function () {
+  const selectedSize = pageSizesArr.find(size => {
+    return size.value == this.value
+  })
+
+  if (selectedSize) {
+    page.style.height = selectedSize.height
+    page.style.width = selectedSize.width
+  }
+})
+
+const orientationValue = document.querySelector("#orientation")
+
+orientationValue.addEventListener("change", function () {
+
+  const styles = window.getComputedStyle(page);
+  let width = styles.width;
+  let height = styles.height;
+
+  if (this.value === "landscape") {
+    page.style.width = height
+    page.style.height = width
+  }
+
+  if (this.value === "portrait") {
+    page.style.width = width
+    page.style.height = height
+  }
+
+})
